1eceba29dd36e53cd1d9e53658aa60e612df5fa9
azilemdey <azilem.v.d@gmail.com>
2019-10-03 14:42:54 -0500

    merge

--- src2/main.js
+++ src2/main.js
@@ -1,6 +1,18 @@
 import { changeTmp } from './view-controller/router.js';
 
 const init = () => {
+    <script>
+                var firebaseConfig = {
+                apiKey: "AIzaSyCAT08HmwAU2eKLS9_Cz2SyMkxZSX7dNvI",
+                authDomain: "burger-queen-8dda0.firebaseapp.com",
+                databaseURL: "https://burger-queen-8dda0.firebaseio.com",
+                projectId: "burger-queen-8dda0",
+                storageBucket: "",
+                messagingSenderId: "341525346793",
+                appId: "1:341525346793:web:dda1e5b0016eb7f1917378"
+              };
+              firebase.initializeApp(firebaseConfig);
+            </script>
     changeTmp(window.location.hash);
     window.addEventListener('hashchange', () => changeTmp(window.location.hash));
 };
