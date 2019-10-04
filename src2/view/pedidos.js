eb8f4de44c577b363821a217f69180a5ff76f2c3
azilemdey <azilem.v.d@gmail.com>
2019-10-03 21:03:51 -0500

    avances para generar el pedido

--- src2/view/pedidos.js
+++ src2/view/pedidos.js
@@ -1,10 +1,15 @@
 export default () => {
-    const viewAccessories = `
-  <h2 class="text-center">PEDIDOS</h2>
-  <div><button>DESAYUNO</button><button>ALMUERZO Y CENA</button></div><div><button>CAFE AMERICANO</button><button>CAFE CON LECHE</button><button>SANDWICH DE JAMON Y QUESO</button><button>JUGO NATURAL</button>`;
-
-    const divElemt = document.createElement('div');
+  const divElemt = document.createElement('div');
     divElemt.classList.add('position')
-    divElemt.innerHTML = viewAccessories;
-    return divElemt;
+    
+  const viewAccessories = `
+  <h2 class="text-center">PEDIDOS</h2>
+  <button id= "desayunos">DESAYUNO</button><div id="desaparece" class="hide"><button>CAFE AMERICANO</button><button>CAFE CON LECHE</button><button>SANDWICH DE JAMON Y QUESO</button><button>JUGO NATURAL</button></div>`;
+  divElemt.innerHTML = viewAccessories;
+
+  const desayunos = divElemt.querySelector('#desayunos');
+  desayunos.addEventListener('click',()=>{
+  const desaparece= divElemt.querySelector('#desaparece');
+  desaparece.classList.remove('hide')});
+  return divElemt;
 };
