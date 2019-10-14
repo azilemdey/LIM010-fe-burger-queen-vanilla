be9629552134615840555e69dcc02f547b660a8f
SisleyMore <sdrmy.20@gmail.com>
2019-10-14 18:59:34 -0500

    añadiendo propiedad cantidad al array

--- src2/view/pedidos.js
+++ src2/view/pedidos.js
@@ -7,27 +7,35 @@ export default () => {
     
   const viewAccessories = `
   <h2 class="text-center">PEDIDOS</h2>
-  <div><button id= "desayunos">DESAYUNO</button><button id= "btn-menus">ALMUERZO Y CENA</button><div id= "carta"></div>`;
+  <div><button id= "desayunos">DESAYUNO</button><button id= "btn-menus">ALMUERZO Y CENA</button><div id= "carta"></div></div>`;
   divElemt.innerHTML = viewAccessories;
   
   const arrPedidos = [];
   const carta = divElemt.querySelector('#carta');
-
+  
   const pintarColeccion = (doc) => {
     let btnName = document.createElement('button');
-
-    // btnName.setAttribute('id', doc.id);
-  
+    btnName.setAttribute('id', doc.id);
     btnName.textContent = `${doc.data().name}:  $${doc.data().precio}`;
-     
     carta.appendChild(btnName);
-
-    btnName.addEventListener('click',()=>{
+    btnName.addEventListener('click',() => {
+      const productoSeleccionado = doc.data();
       arrPedidos.push(doc.data());
-      console.log(arrPedidos);
-      
-      
-    });
+      if (arrPedidos.length === 0 ) {
+        productoSeleccionado.cantidad = 1;
+        console.log(productoSeleccionado);
+        console.log(doc.id);
+      }else {
+       const arr = arrPedidos.find(producto => producto.id === doc.id);
+       console.log(arr);
+       
+      }  
+      });
+      // else if (doc.id === doc.data().id) {
+        
+      // }
+     
+
   }
   const desayunos = divElemt.querySelector('#desayunos');
 
