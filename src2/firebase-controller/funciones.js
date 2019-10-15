7a3e3c7583e6eb67a4c4cf516f094f1ff6407944
azilemdey <azilem.v.d@gmail.com>
2019-10-15 17:58:54 -0500

    dando diseño a la impresion

--- src2/firebase-controller/funciones.js
+++ src2/firebase-controller/funciones.js
@@ -1,3 +1,12 @@
 export const getData = (coleccionName) => {
   return  firebase.firestore().collection(coleccionName).get();
 };
+
+
+//para separar
+export const pintarArray = (arrayPrint, eleHTML) => {
+  eleHTML.innerHTML = '';
+  const resultado = arrayPrint.reduce((accum, elem) => {
+    return accum + `<p>${elem.name}  $${elem.precio}  ${elem.cantidad} unidades</p>`;
+  }, '')
+  eleHTML.innerHTML = resultado};
