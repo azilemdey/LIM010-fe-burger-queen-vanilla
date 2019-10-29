ec9cb515598f7f6623d766c938945f87fe92b7cf
SisleyMore <sdrmy.20@gmail.com>
2019-10-29 17:37:57 -0500

    intentando pintar adicionales

--- src2/view/pedidos.js
+++ src2/view/pedidos.js
@@ -1,5 +1,5 @@
 import { getData} from "../functions-controller/firebase-controller.js";
-import { pintarArray} from "../functions-controller/views-controller.js";
+import { pintarArray, listas} from "../functions-controller/views-controller.js";
 import{aumentarCantidad} from "../functions-controller/funciones.js";
 
 export default () => {
@@ -9,7 +9,7 @@ export default () => {
   const viewAccessories = `
   <h2 class="text-center">PEDIDOS</h2>
   <div><button id= "desayunos">DESAYUNO</button><button id= "btn-menus">ALMUERZO Y CENA</button><div id= "carta"></div>
-  </div><input class= "hide" type = "text"><div id= "area-pedidos" class="hide"><div id="precio-total"></div></div>`;
+  </div><ul class = "hide" id = "adicional"><li id= "lista"></li></ul><input class= "hide" type = "text"><div id= "area-pedidos" class="hide"><div id="precio-total"></div></div>`;
   divElemt.innerHTML = viewAccessories;
   const carta = divElemt.querySelector('#carta');
 
@@ -27,17 +27,28 @@ export default () => {
       console.log(copiaObj);
       
       if (copiaObj.name === "Hamburguesa simple" || copiaObj.name === "Hamburguesa doble") {
-        alert('jnhn');
-        // const sabores = doc.data().sabores;
-        // sabores.forEach(sabor => {
-          // const listaSabores = document.createElement("ul");
-          // const lista = document.createComment("li");
-          // lista.appendChild(listaSabores);
-          // listaSabores.appendChild(btnName);
-          // lista.textContent =  doc.data().sabores;
-      //   })
+        // alert('jnhn');
+        const sabores = doc.data().sabores;
+        
+
       // console.log(doc.data().sabores);
-     
+
+      // const lista = document.createElement("li");
+      // adicional.appendChild(lista);
+      // sabores.forEach(sabor => {
+      //   const adicional = divElemt.querySelector('#adicional');   
+      //   const lista = divElemt.querySelector('#lista');  
+      //   adicional.classList.remove('hide');
+      //   lista.classList.remove('hide');
+
+       
+
+        // const adicional = divElemt.querySelector('#adicional');   
+        // const lista = divElemt.querySelector('#lista');  
+        // adicional.classList.remove('hide');
+        // lista.classList.remove('hide');
+        // lista.textContent = listaS;
+        
        }
      aumentarCantidad(copiaObj,doc.id);
      let areaPedidos = divElemt.querySelector('#area-pedidos');   
